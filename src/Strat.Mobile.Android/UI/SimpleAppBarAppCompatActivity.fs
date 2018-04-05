e1334c12f1e13e52887d13358cda438d0934507d
jlewicki <jlewicki@gmail.com>
2018-04-05 06:16:03 -0400

    Remove view model stuff.

--- src/Strat.Mobile.Android/UI/SimpleAppBarAppCompatActivity.fs
+++ src/Strat.Mobile.Android/UI/SimpleAppBarAppCompatActivity.fs
@@ -1,40 +1,22 @@
 ﻿namespace Strat.Mobile.Android.UI
 
 open System
-open System.Reactive.Disposables
 open System.Reactive.Linq
 open System.Reactive.Subjects
 open Android.Views
 open Android.Support.V7.Widget
 open Strat.Mobile.Android.DI.SimpleInjector.AppCompat
-open Strat.UI
 
 
+/// Helper class with some methods for helping simplify working with action bar.
+// TODO: this may not be suitable for a library...
 [<AbstractClass>]
-type ViewModelAppCompatActivity<'ViewModel> () =
+type SimpleAppBarAppCompatActivity () =
    // TODO: don't use this base class
    inherit SimpleInjectorAppCompatActivity()
-
-   let mutable _viewModel = Unchecked.defaultof<'ViewModel>
+  
    let mutable _actionBarMenuItems: option<ResourceOrValue<list<MenuItem>>> = None
-   let mutable _bindingDisposable = Disposable.Empty
    let _optionsItemSelectedSubject = new Subject<int>()
-
-   interface IView<'ViewModel> with 
-      // TODO: Should we be providing an Observable<'ViewModel> instead?
-      member this.SetViewModel vm = 
-         // Dispose of any subscriptions the subclass has made to avoid memory leaks
-         _bindingDisposable.Dispose()
-         _viewModel <- vm 
-         // Bind to the new view model, capturing any subscriptions made by the subclass
-         _bindingDisposable <- new CompositeDisposable(this.BindViewModel(vm))
-
-
-   /// Called when this activity should data bind to the specified view model. Subclasses should return a sequence of
-   /// disposables representing subscriptions to events from the view model, or views loaded by the activity. These
-   /// subscriptions will be destroyed before this method is called again with a new view model. 
-   abstract BindViewModel: viewModel: 'ViewModel -> seq<IDisposable>
-   
 
    /// Returns an observable that yields each time the user selects the menu intem with the specfied ID.
    member this.OptionItemSelected (menuItemId: int) : IObservable<unit> = 
@@ -93,12 +75,13 @@ type ViewModelAppCompatActivity<'ViewModel> () =
    /// Yields values from the OptionsItemSelected observable as items are selected by the user.
    override this.OnOptionsItemSelected menuItem = 
       if menuItem.ItemId = Android.Resource.Id.Home then 
+         // If user hits Home/Up option, just let base class handle it, which will subsequenty call OnSupportNavigateUp
          base.OnOptionsItemSelected menuItem
       else
          _optionsItemSelectedSubject.OnNext menuItem.ItemId
          true
 
-   // Called when user hits back button in activity toolbar.
-   override this.OnNavigateUp() = 
+   // Called when user hits Home/Up option button in activity toolbar.
+   override this.OnSupportNavigateUp() = 
       this.OnBackPressed()
       false
